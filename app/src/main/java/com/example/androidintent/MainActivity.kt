b283de26624aa57d05f175cc29957a34f505d91e
eunjung04 <peunj04@gmail.com>
2020-04-09 20:47:26 +0900

    다이얼 인텐트 예제

--- app/src/main/java/com/example/androidintent/MainActivity.kt
+++ app/src/main/java/com/example/androidintent/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.androidintent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+    }
+
+
+    fun setupEvents() {
+
+        dialBtn.setONClickListener{
+
+            val myUri = Uri.parse("tel:01026816612")
+            val myIntent=Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+
+
+
+        }
+
+
+
     }
 }
